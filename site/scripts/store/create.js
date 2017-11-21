96549ccf8847531d0d4ebc0ee86afbff5d44b823
Harry Sarson <harry.sarson@hotmail.co.uk>
2017-11-21 14:46:18 +0000

    introduces rxjs to observe changes to state

--- site/scripts/store/create.js
+++ site/scripts/store/create.js
@@ -4,8 +4,29 @@ const { createStore } = Redux;
 import reducers from '../reducers/index.js'
 
 export default function () {
-  return createStore(
+  const store = createStore(
     reducers,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   );
+
+  const subject = new Rx.Subject();
+
+  subject.next(store.getState);
+
+  const observable = Rx.Observable.create(function subscribe(observer) {
+    const unsubscribe_redux = store.subscribe(function() {
+      observer.next(store.getState());
+    });
+
+    observer.next(store.getState());
+
+    return function unsubscribe() {
+      unsubscribe_redux();
+    }
+  });
+
+  // todo find way to avoid monkey patch
+  observable.dispatch = (...args) => store.dispatch(...args);
+
+  return observable;
 }
