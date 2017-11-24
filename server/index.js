0708e3d63c395a1d93bd5b592a639b2bea7afd35
Harry Sarson <harry.sarson@hotmail.co.uk>
2017-11-24 14:50:34 +0000

    fixes linting

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
+/* eslint consistent-return: "off" */
+
 const path = require('path');
 
 const Koa = require('koa');
 const getPkgDir = require('pkg-dir');
-const getRawBody = require('raw-body')
 const serve = require('koa-static');
 const koaBody = require('koa-body');
-const route = require('koa-route');
 const Debug = require('debug');
 const os = require('os');
 const fs = require('fs');
@@ -14,31 +14,29 @@ const pkgDir = getPkgDir.sync(__dirname);
 const app = new Koa();
 
 const debug = Debug('server');
-  
 
 
 app
   .use(serve(path.join(pkgDir, 'site')))
 
   .use(koaBody({ multipart: true }))
-  .use(async function(ctx, next) {
+  .use(async function process(ctx, next) {
     // ignore non-POSTs
-    if ('POST' != ctx.method) return await next();
-    
+    if (ctx.method !== 'POST') return next();
+
     debug(ctx.request.body);
-    
-    const file = ctx.request.body.files.file;
+
+    const { request: { body: { files: { file } } } } = ctx;
     const reader = fs.createReadStream(file.path);
-    const stream = fs.createWriteStream(path.join(os.tmpdir(), Math.random().toString() + file.name));
+    const stream = fs.createWriteStream(path.join(
+      os.tmpdir(),
+      Math.random().toString() + file.name,
+    ));
     reader.pipe(stream);
     debug('uploading %s -> %s', file.name, stream.path);
 
     ctx.redirect('/');
   });
- 
-  
-;
-
 
 
 app.listen(3000);
