3176d39cb52376c028f3e4dbf29b8a9ca0074b24
Harry Sarson <harry.sarson@hotmail.co.uk>
2017-11-24 14:50:34 +0000

    fixes linting

--- site/scripts/store/create.js
+++ site/scripts/store/create.js
@@ -4,6 +4,7 @@ const { Redux: { createStore } } = window;
 const { Rx } = window;
 
 export default function () {
+  /* eslint no-underscore-dangle: ["warn"] */
   const store = createStore(
     reducers,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
