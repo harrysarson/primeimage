44833e843c721f6979684855884ed6a3f747a7f6
Harry Sarson <harry.sarson@hotmail.co.uk>
2017-11-24 21:01:03 +0000

    adds scripts to handle loading image into site

---
+++ site/scripts/lib/file-read.js
@@ -0,0 +1,32 @@
+
+const createReaderPromise = (file, func) => new Promise(function executor(resolve, reject) {
+  const reader = new FileReader();
+
+  reader.onload = function onLoad() {
+    resolve(reader.result);
+  };
+
+  function onError() {
+    reject(reader.error);
+  }
+
+  reader.onabort = onError;
+  reader.onerror = onError;
+
+  func.call(reader, file);
+});
+
+export default class FileRead {
+  constructor(file) {
+    Object.defineProperties(this, {
+      file: {
+        enumerable: true,
+        value: file,
+      },
+    });
+  }
+
+  asDataURL() {
+    return createReaderPromise(this.file, FileReader.prototype.readAsDataURL);
+  }
+}
