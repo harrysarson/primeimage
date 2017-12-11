e75873b8e5a5b60e0ae98933a415ac280ae56027
Harry Sarson <harry.sarson@hotmail.co.uk>
2017-12-11 20:45:27 +0000

    adds image load actions

--- site/scripts/actions/index.js
+++ site/scripts/actions/index.js
@@ -1,7 +1,11 @@
+import ReadFile from '../lib/ReadFile.js';
+import loadImg from '../lib/loadImg.js';
 
 export const types = [
   'moveStage',
   'setStage',
+  'selectFile',
+  'loadFile',
 ].reduce((obj, type) => Object.assign(obj, { [type]: type }), {});
 
 export const creators = {
@@ -13,6 +17,49 @@ export const creators = {
     type: types.setStage,
     newStage,
   }),
+  selectFile: file => ({
+    type: types.selectFile,
+    file,
+  }),
+  loadFile: Object.assign(
+    function loadFile(file) {
+      return async (dispatch) => {
+        try {
+          dispatch(loadFile.begin(file));
+
+          if (!file) {
+            throw new Error('null file provided');
+          }
+
+          const dataUrl = await new ReadFile(file).asDataURL(file);
+
+          const img = await loadImg(dataUrl);
+
+          return dispatch(loadFile.complete(img));
+        } catch (err) {
+          dispatch(loadFile.error(err));
+          throw err;
+        }
+      };
+    },
+    {
+      begin: file => ({
+        type: types.loadFile,
+        status: 'begin',
+        file,
+      }),
+      error: error => ({
+        type: types.loadFile,
+        status: 'error',
+        error,
+      }),
+      complete: img => ({
+        type: types.loadFile,
+        status: 'end',
+        img,
+      }),
+    },
+  ),
 };
 
 Object.freeze(types);
