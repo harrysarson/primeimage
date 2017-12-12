d258beb9b6d1ee62886d2f584030866d11610b69
Harry Sarson <harry.sarson@hotmail.co.uk>
2017-12-12 16:09:35 +0000

    adds image loading stage

---
+++ site/scripts/gui/stage/imageLoad.js
@@ -0,0 +1,115 @@
+import { Subject } from '../../deps/rxjs/Subject.js';
+import { pluck, tap, map, filter } from '../../deps/rxjs/operators.js';
+
+import { creators as actionCreators } from '../../actions/index.js';
+
+
+function createDragDrop($element) {
+  function addListener(eventNames, cb) {
+    for (const eventName of eventNames) {
+      $element.addEventListener(eventName, cb);
+    }
+  }
+
+  addListener(
+    ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'],
+    function onDragEvent(e) {
+      // preventing the unwanted behaviours
+      e.preventDefault();
+      e.stopPropagation();
+    },
+  );
+  addListener(
+    ['dragover', 'dragenter'],
+    function onDragStart() {
+      $element.classList.add('is-dragover');
+    },
+  );
+  addListener(
+    ['dragleave', 'dragend', 'drop'],
+    function onDragEnd() {
+      $element.classList.remove('is-dragover');
+    },
+  );
+
+  // todo replace with Rx.Observable.fromEvent
+
+  const subject = new Subject();
+  const input = $element.querySelector('input[type="file"]');
+
+  $element.addEventListener('submit', function onSubmit(e) {
+    $element.preventDefault();
+
+    subject.next(e.target.files);
+  });
+
+  input.addEventListener('change', function onChange(e) {
+    subject.next(e.target.files);
+  });
+
+  $element.addEventListener('drop', function onDrop(e) {
+    subject.next(e.dataTransfer.files);
+  });
+
+  return subject;
+}
+
+const imgDrawer = canvas => function drawImgOnCanvas(img) {
+  const aspectRatio = img.width / img.height;
+
+  Object.assign(canvas.style, { width: '100%', height: '100%' });
+
+  const maxHeight = canvas.offsetHeight;
+  const maxWidth = canvas.offsetWidth;
+
+  Object.assign(canvas.style, { width: '', height: '' });
+
+  let height;
+  let width;
+
+  if (maxHeight * aspectRatio > maxWidth) {
+    height = maxWidth / aspectRatio;
+    width = maxWidth;
+  } else {
+    height = maxHeight;
+    width = maxHeight * aspectRatio;
+  }
+
+  Object.assign(canvas, { width, height });
+
+  const ctx = canvas.getContext('2d');
+  ctx.imageSmoothingEnabled = false;
+
+  ctx.drawImage(img, 0, 0, width, height);
+};
+
+
+/**
+ * Creates GUI for buttons that change the current stage.
+ *
+ * @param {object} args
+ * @param {Element} args.attributename Name of HTML attribute defining the button.
+ * @param {number} maxStage Index of maximum stage.
+ *
+ * @returns Object allowing buttons to be observed and updated.
+ */
+export default () => Object.freeze({
+  observable($input) {
+    return createDragDrop($input).pipe(
+      map(files => files[0]),
+      map(actionCreators.loadFile),
+    );
+  },
+
+  updater($output) {
+    return (source) => {
+      const canvas = $output.querySelector('canvas');
+
+      return source.pipe(
+        pluck('img'),
+        filter(img => img != null),
+        tap(imgDrawer(canvas)),
+      );
+    };
+  },
+});
