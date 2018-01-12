dd4edd1887d37258dae914aa6424431d1410bc2d
Harry Sarson <harry.sarson@hotmail.co.uk>
2018-01-12 21:44:32 +0000

    Adds test for loadImg error handling

--- site/scripts/lib/loadImg.js
+++ site/scripts/lib/loadImg.js
@@ -1,15 +1,20 @@
 
 // todo errors?
 
-export function loadImg(src, doc = document) {
-  const img = doc.createElement('img');
+export function loadImg(src) {
+  const img = document.createElement('img');
 
   return new Promise(function executor(resolve, reject) {
     img.onload = function onLoad() {
       resolve(img);
     };
 
-    img.onerror = function onError(error) {
+    img.onerror = function onError(event) {
+      const error = Object.assign(new Error('Could not load img'), {
+        img: event.target,
+        source: src,
+      });
+
       reject(error);
     };
 
