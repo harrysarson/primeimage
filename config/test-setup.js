80fcd4c586f9797baf732f86a9e4059df1a3086b
Harry Sarson <harry.sarson@hotmail.co.uk>
2018-01-12 21:44:32 +0000

    Adds test for loadImg error handling

--- config/test-setup.js
+++ config/test-setup.js
@@ -4,5 +4,6 @@ const chai = require('chai');
 chai.use(require('chai-iterator'));
 chai.use(require('sinon-chai'));
 chai.use(require('chai-dom'));
+chai.use(require('chai-as-promised'));
 
 global.expect = chai.expect;
