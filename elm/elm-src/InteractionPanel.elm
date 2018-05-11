fa4199f16d05227e431a23ba6790ec9fe3e112bb
Harry Sarson <harry.sarson@hotmail.co.uk>
2018-05-11 17:27:13 +0100

    add file picker UI to interaction panel

--- elm/elm-src/InteractionPanel.elm
+++ elm/elm-src/InteractionPanel.elm
@@ -2,7 +2,9 @@ module InteractionPanel exposing (Props, view)
 
 import Array
 import Html exposing (..)
+import Svg
 import Html.Attributes exposing (..)
+import Svg.Attributes
 import Html.Events exposing (..)
 
 import Types
@@ -18,7 +20,8 @@ view props =
     [ section
       [ class "interaction-instructions stage-selecting" ]
       (Maybe.withDefault (default_instructions props.stage) (Array.get props.stage instructions))
-    , div [ class "interaction-interface stage-selecting show-only-selected" ] []
+    , div [ class "interaction-interface stage-selecting" ]
+      (Maybe.withDefault [] (Array.get props.stage interactions))
     , div [ class "interaction-control" ]
       [ button
         [ attribute "data-stage-change" "-1"
@@ -44,6 +47,45 @@ instructions = Array.fromList
     , p [] [ text "Use the box below to open an image." ]
     ]
   ]
+
+-- todo: xmlns="http://www.w3.org/2000/svg"
+
+-- todo: report runtime exception using html class with svg
+
+interactions = Array.fromList
+  [ []
+  , [ Html.form [ class "image-pick" ]
+      [ Svg.svg [ Svg.Attributes.class "icon", Svg.Attributes.width "50", Svg.Attributes.height "43", Svg.Attributes.viewBox "0 0 50 43" ]
+        [ Svg.path
+          [ Svg.Attributes.d """
+              M48.4 26.5c-.9 0-1.7.7-1.7 1.7v11.6h-43.3v-11.6c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v13.2c0
+              .9.7 1.7 1.7 1.7h46.7c.9 0 1.7-.7 1.7-1.7v-13.2c0-1-.7-1.7-1.7-1.7zm-24.5 6.1c.3.3.8.5
+              1.2.5.4 0 .9-.2 1.2-.5l10-11.6c.7-.7.7-1.7 0-2.4s-1.7-.7-2.4
+              0l-7.1 8.3v-25.3c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v25.3l-7.1-8.3c-.7-.7-1.7-.7-2.4
+              0s-.7 1.7 0 2.4l10 11.6z
+              """
+          ] []
+        ]
+      , input [ type_ "file", name "files[]", id "file" ] []
+      , label [ for "file" ]
+        [ strong [] [ text "Choose a file" ]
+        , text " or drag it here."
+        ]
+      ]
+    ]
+  ]
+
+{-
+
+      <form class="image-pick">
+        <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="50" height="43" viewBox="0 0 50 43">
+          <path d="M48.4 26.5c-.9 0-1.7.7-1.7 1.7v11.6h-43.3v-11.6c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v13.2c0 .9.7 1.7 1.7 1.7h46.7c.9 0 1.7-.7 1.7-1.7v-13.2c0-1-.7-1.7-1.7-1.7zm-24.5 6.1c.3.3.8.5 1.2.5.4 0 .9-.2 1.2-.5l10-11.6c.7-.7.7-1.7 0-2.4s-1.7-.7-2.4 0l-7.1 8.3v-25.3c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v25.3l-7.1-8.3c-.7-.7-1.7-.7-2.4 0s-.7 1.7 0 2.4l10 11.6z">
+          </path>
+        </svg>
+        <input type="file" name="files[]" id="file" />
+        <label for="file"><strong>Choose a file</strong> or drag it here.</label>
+      </form>
+  -}
 
 default_instructions : int -> List(Html msg)
 default_instructions stage =
--- elm/tests/InteractionPanelSpec.elm
+++ elm/tests/InteractionPanelSpec.elm
@@ -1,10 +1,11 @@
 module InteractionPanelSpec exposing (..)
 
 import Random.Pcg as Random
+import Html.Attributes as Attr
 
 import Test exposing (describe, test, fuzz)
 import Test.Html.Query as Query
-import Test.Html.Selector exposing (text, tag, class, classes)
+import Test.Html.Selector exposing (id, tag, class, attribute)
 import Test.Html.Event as Event
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer)
@@ -32,9 +33,9 @@ tests =
           [ Query.has [class "interaction-panel"]
           , Query.children []
             >> queryInOrder
-              [ Query.has [class "interaction-instructions"]
-              , Query.has [class "interaction-interface"]
-              , Query.has [class "interaction-control"]
+              [ Query.has [ class "interaction-instructions" ]
+              , Query.has [ class "interaction-interface" ]
+              , Query.has [ class "interaction-control" ]
               ]
           ]
     , fuzz props "Should have header" <|
@@ -42,6 +43,22 @@ tests =
         >> Query.fromHtml
         >> Query.find [ tag "h1" ]
         >> \_ -> Expect.pass
+    , test "Stage 0 should have no interaction" <|
+        \() -> view { stage = 0, change = (\i -> Click i) }
+          |> Query.fromHtml
+          |> Query.find [ class "interaction-interface" ]
+          |> Query.children []
+          |> Query.count (Expect.equal 0)
+    , test "Stage 1 should have file loader interaction" <|
+        \() -> view { stage = 1, change = (\i -> Click i) }
+          |> Query.fromHtml
+          |> Query.find [ class "interaction-interface" ]
+          |> Query.find [ tag "form" ]
+          |> Expect.all
+            [ Query.has [ class "image-pick" ]
+            , Query.find [ tag "input" ]
+              >> Query.has [ attribute (Attr.type_ "file"), attribute (Attr.name "files[]"),   id "file" ]
+            ]
     , test "Should have back and forwards button" <|
         \() -> { stage = 2, change = (\i -> Click i) }
         |> view
