d550cd7302274857dd8b6a3cf42420fb84212d3d
Harry Sarson <harry.sarson@hotmail.co.uk>
2018-05-17 16:25:42 +0100

    elm only repo, ignore node_modules

--- binding.gyp
+++
@@ -1,8 +0,0 @@
-{
-  "targets": [
-    {
-      "target_name": "addon",
-      "sources": [ "lib/addon.cc" ]
-    }
-  ]
-}
--- site/_scripts/lib/pipe.js
+++
@@ -1,5 +0,0 @@
-
-export const pipe = (initialValue, ...projects) => projects.reduce(
-  (value, project) => project(value),
-  initialValue,
-);
