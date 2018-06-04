753494e1a99c6e30a95dbcd4b78b53eae48dad04
Harry Sarson <harry.sarson@hotmail.co.uk>
2018-06-04 22:45:08 +0100

    test ToNumberConfig
    Adds tests for the State, Types and the View.

--- elm-tests/Util.elm
+++ elm-tests/Util.elm
@@ -1,4 +1,7 @@
-module Util exposing (queryInOrder)
+module Util exposing (queryInOrder, stringGen)
+
+import Random.Pcg as Random exposing (Generator)
+import Char
 
 import Test.Html.Query exposing (Single, Multiple, index, count)
 import Expect exposing (Expectation)
@@ -12,3 +15,28 @@ queryInOrder expectors =
       (\i -> \expector -> index i >> expector)
       expectors
     )
+
+
+{-| Generate a random string of a given length with a given character generator
+    fiveLetterEnglishWord = string 5 Random.Char.english
+-}
+stringOfLengthGen : Int -> Generator String
+stringOfLengthGen stringLength =
+    Random.map
+        String.fromList
+        (Random.list stringLength
+          (Random.map
+              Char.fromCode
+              (Random.int 0 65535)
+          )
+        )
+
+
+{-| Generates a random string of random length given the minimum length
+and maximum length and a given character generator.
+-}
+stringGen : Int -> Int -> Generator String
+stringGen minLength maxLength =
+    Random.andThen
+        (\len -> stringOfLengthGen len    )
+        (Random.int minLength maxLength)
