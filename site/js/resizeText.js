653e8da775a7deb81c2a219f506911c1a5922501
Harry Sarson <harry.sarson@hotmail.co.uk>
2018-06-12 16:03:53 +0100

    improve resizing of text!

---
+++ site/js/resizeText.js
@@ -0,0 +1,12 @@
+export function childFits($node, $parent) {
+  const parentDim = {
+    width: $parent.clientWidth,
+    height: $parent.clientHeight,
+  };
+  const textDim = {
+    width: $node.offsetWidth,
+    height: $node.offsetHeight,
+  };
+
+  return textDim.width < parentDim.width && textDim.height < parentDim.height;
+}
