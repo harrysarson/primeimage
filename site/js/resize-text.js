55b06cfd113eda368d55f941727aa26f91464544
Harry Sarson <harry.sarson@hotmail.co.uk>
2018-06-20 14:23:15 +0100

    move functions out of index.js

--- site/js/resize-text.js
+++ site/js/resize-text.js
@@ -10,3 +10,26 @@ export function childFits($node, $parent) {
 
   return textDim.width < parentDim.width && textDim.height < parentDim.height;
 }
+
+export function resizeText($node) {
+  // TODO: check not null
+  const $parent = $node.parentNode;
+
+  let intFontSize = parseInt(getComputedStyle($node).getPropertyValue('font-size'), 10);
+
+  if (isNaN(intFontSize)) {
+    throw new TypeError('Invalid font size read');
+  }
+
+  /* Increase font size util it does not fit */
+
+  for (; intFontSize < 100 && childFits($node, $parent); ++intFontSize) {
+    $node.style.setProperty('font-size', intFontSize + 'px');
+  }
+
+  /* Decrease font size to fit */
+
+  for (; intFontSize > 0 && !childFits($node, $parent); --intFontSize) {
+    $node.style.setProperty('font-size', intFontSize + 'px');
+  }
+}
