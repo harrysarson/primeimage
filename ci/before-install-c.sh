1f6a9072c7cf0912dc2b2e722d76bdadc0923f7a
Harry Sarson <harry.sarson@hotmail.co.uk>
2019-04-22 10:07:07 +0100

    Use bash script for before install

--- ci/before-install-c.sh
+++ ci/before-install-c.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
+set -e
+
 # Credit: https://github.com/TrueBitFoundation/wasm-ports/
 
-mkdir -p build
-cd build
+mkdir -p ci/build
+cd ci/build
 
 # download
 
-wget https://gmplib.org/download/gmp/gmp-6.1.2.tar.lz
-tar xf gmp-6.1.2.tar.lz
-cd gmp-6.1.2
+if [[ ! -d gmp-native ]]; then
+    wget https://gmplib.org/download/gmp/gmp-6.1.2.tar.lz
+    tar xf gmp-6.1.2.tar.lz -C gmp-native
+fi
+
+if [[ ! -d gmp-wasm ]]; then
+    cp gmp-native gmp-wasm -r
+fi
+
 
 # Make normally
 
+cd gmp-native
 ./configure
 make -j 2
 sudo make install
-
-sed -i '2igmp_asm_syntax_testing=no' configure
+cd -
 
 # Make webassembly
+
+cd gmp-wasm
+sed -i '2igmp_asm_syntax_testing=no' configure
 
 emconfigure ./configure --disable-assembly --disable-shared --prefix=$EMSCRIPTEN/system
 
@@ -30,7 +41,5 @@ echo '#define HAVE_VSNPRINTF 1' >> config.h
 make -j 2
 make install
 
-# clean up
-
-cd ..
+cd -
 
