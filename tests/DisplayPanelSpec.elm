c772b299a7181b4102030b9a438022b47ecbf2cb
Harry Sarson <harry.sarson@hotmail.co.uk>
2019-05-26 23:33:32 +0100

    update tests for menu bar

--- tests/DisplayPanelSpec.elm
+++ tests/DisplayPanelSpec.elm
@@ -69,9 +69,11 @@ tests =
                 >> Expect.all
                     [ Query.children []
                         >> Expect.all
-                            [ Query.count (Expect.equal 1)
-                            , Query.first
+                            [ Query.count (Expect.equal 2)
+                            , Query.index 0
                                 >> Query.has [ class "display-panel" ]
+                            , Query.index 1
+                                >> Query.has [ class "menu-bar" ]
                             ]
                     ]
         , fuzz props "Displays should have the correct contents" <|
