392baa91f6ce65ba869cf84da9039a7c05e9e475
Harry Sarson <harry.sarson@hotmail.co.uk>
2019-08-22 18:34:57 +0100

    get incremental search prepared
    most of the prime search organisation is now in javascript

--- site/js/prime-search-worker.js
+++ site/js/prime-search-worker.js
@@ -12,20 +12,20 @@ const PrimeSearch = exports.prime_search({
   }
 });
 
-onmessage = function ({data: payload}) {
+onmessage = function ({ data: payload }) {
   PrimeSearch.then(() => {
     switch (payload.type) {
       case 'Start': {
         const reps = 10;
-        const {nonPrimeNumber} = payload;
+        const { nonPrimeNumber } = payload;
 
         const allocAmount = PrimeSearch.lengthBytesUTF8(nonPrimeNumber) + 1;
         const buffer = PrimeSearch._malloc(allocAmount);
 
         try {
           PrimeSearch.stringToUTF8(nonPrimeNumber, buffer, allocAmount);
-          // const res = PrimeSearch._find_candidate_with_progress(buffer, reps);
-          const res = find_candidate_with_progress(nonPrimeNumber, reps);
+          // Const res = PrimeSearch._find_candidate_with_progress(buffer, reps);
+          const [res, primeNumber] = find_candidate_with_progress(nonPrimeNumber, reps);
           switch (res) {
             case 1:
             case 2: {
@@ -33,7 +33,7 @@ onmessage = function ({data: payload}) {
               postMessage({
                 type: 'FoundPrime',
                 log2ProbPrime,
-                primeNumber: PrimeSearch.UTF8ToString(buffer) // eslint-disable-line new-cap
+                primeNumber, // : PrimeSearch.UTF8ToString(buffer) // eslint-disable-line new-cap
               });
               break;
             }
@@ -70,32 +70,27 @@ onmessage = function ({data: payload}) {
   });
 };
 
+function find_candidate_with_progress(input, reps) {
+  let res = [0, ''];
 
-function find_candidate_with_progress(input, reps)
-{
   const trimmed = input.trim();
   const len = PrimeSearch.lengthBytesUTF8(trimmed);
   const str = PrimeSearch._malloc(len + 1);
 
   // Create bitmask with more bits in than there are bytes in input.
-  let bitmask = PrimeSearch._malloc(PrimeSearch._bitset_size(len));
+  const bitmask = PrimeSearch._malloc(PrimeSearch._bitset_size(len));
 
   try {
-    PrimeSearch.stringToUTF8(trimmed, str, allocAmount);
+    PrimeSearch.stringToUTF8(trimmed, str, len + 1);
 
     console.warn(`Comencing search for a prime candidate, target is ${len} digits long.`);
 
-    for (let i = 1; i < len; i++)
-    {
+    for (let i = 1; i < len; i++) {
       // Set first j i bits of bitmask.
-      for (let j = 0; j < len; j++)
-      {
-        if (j < i)
-        {
+      for (let j = 0; j < len; j++) {
+        if (j < i) {
           PrimeSearch._bitset_set(bitmask, j, 1);
-        }
-        else
-        {
+        } else {
           PrimeSearch._bitset_set(bitmask, j, 0);
         }
       }
@@ -104,45 +99,39 @@ function find_candidate_with_progress(input, reps)
 
       let total_iteration_count = 0;
 
-      console.warn(progressToString(i, total_iteration_count, numberOfPerumations));
+      console.warn(progressToString(i, total_iteration_count, numberOfPermuations));
 
       let find_res;
-      do
-      {
+      do {
         find_res = PrimeSearch._find_candidate_using_bitmask(str, reps, bitmask, MAX_ITERATIONS_BETWEEN_PRINT);
         total_iteration_count += MAX_ITERATIONS_BETWEEN_PRINT;
-        console.warn(progressToString(i, total_iteration_count, numberOfPerumations));
-      } while (find_res == 0);
+        console.warn(progressToString(i, total_iteration_count, numberOfPermuations));
+      } while (find_res === 0);
 
-      if (find_res == 1 || find_res == 2)
-      {
-        res = find_res;
-        strcpy(input, str);
-        fprintf(stderr, ": Found prime!\n");
+      if (find_res === 1 || find_res === 2) {
+        res = [find_res, PrimeSearch.UTF8ToString(str)]; // eslint-disable-line new-cap
+        console.warn(': Found prime!');
         break;
-      }
-      else if(find_res == -1) {
-        console.warn(": Could not find any primes.");
+      } else if (find_res === -1) {
+        console.warn(': Could not find any primes.');
       }
     }
   } finally {
     PrimeSearch._free(bitmask);
     PrimeSearch._free(str);
   }
+
   return res;
 }
 
-function nCr(n, r)
-{
-  if (r > n - r)
-  {
-    r = n - r; // because C(n, r) == C(n, n - r)
+function nCr(n, r) {
+  if (r > n - r) {
+    r = n - r; // Because C(n, r) == C(n, n - r)
   }
 
   let ans = 1;
 
-  for (let i = 1; i <= r; i++)
-  {
+  for (let i = 1; i <= r; i++) {
     ans *= n - r + i;
     ans /= i;
   }
